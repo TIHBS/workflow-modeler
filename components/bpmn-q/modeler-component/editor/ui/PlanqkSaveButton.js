e03e6dd6509260bda3a198ad5094dda3196dfef6
nziebura <nikolaus.ziebura@stoneone.de>
2024-03-13 13:13:01 +0100

    add notification for save

--- components/bpmn-q/modeler-component/editor/ui/PlanqkSaveButton.js
+++ components/bpmn-q/modeler-component/editor/ui/PlanqkSaveButton.js
@@ -2,6 +2,7 @@ import React from "react";
 import {
   dispatchWorkflowChangedEvent,
 } from "../util/IoUtilities";
+import NotificationHandler from "./notifications/NotificationHandler";
 
 /**
  * React button which starts sending a changed workflow via event to the planqk platform
@@ -12,6 +13,18 @@ import {
 export default function PlanqkSaveButton(props) {
   const { modeler } = props;
 
+  async function saveToPlanQK() {
+    await dispatchWorkflowChangedEvent(modeler);
+
+    NotificationHandler.getInstance().displayNotification({
+      title: "Workflow saved",
+      content: "You can now safely leave the editor and continue working on the workflow later again." +
+          " ________________________________________________________________________ " +
+          "Note: to execute the changed workflow you have to also deploy it.",
+      duration: 6000,
+    });
+  }
+
   return (
     <div>
       <button className="qwm-toolbar-btn" title="Save workflow">
@@ -19,9 +32,7 @@ export default function PlanqkSaveButton(props) {
           <div style={{display: "flex"}}>
             <span
               className={"qwm-icon-saving"}
-              onClick={() => {
-                dispatchWorkflowChangedEvent(modeler);
-              }}
+              onClick={() => saveToPlanQK() }
             >
             <span className="qwm-indent">Save</span>
           </span>
